e39c9c023b796856ae059ce516bdff0eff57d7d9
FallenX <gyygg2323@gmail.com>
2025-09-27 20:14:48 -0300

    Update Servers.php

--- api/Requests/Servers.php
+++ api/Requests/Servers.php
@@ -5,8 +5,9 @@
 	  "status": 1,
 	  "recommend": true,
 	  "newstatus": false,
-      "edgar_host": "SPACE-RP",
-      "edgar_port": 7777,
+      "edgar_host": "54.38.117.72",
+      "edgar_port": 1177,
       "id": 0
 	}
 ]
+
