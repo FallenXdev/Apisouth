099c1d7d4c4abcd85d8e35bec66714c7ed54267e
FallenX <gyygg2323@gmail.com>
2025-10-19 12:58:22 -0300

    Update connect.php

--- api/Requests/vendor/connect.php
+++ api/Requests/vendor/connect.php
@@ -1,12 +1,10 @@
 <?php
-require_once "rb.php";
-
-$servername = "51.81.57.217"; 
-$dbname = "s3265530_southtestes"; 
-$dbusername = "u3265530_KF5ci9bGmU"; 
-$dbpassword = "7FV!kWuM4mhn1.HdQ.m6Kpz5";
-
-R::setup("mysql:host=$servername;dbname=$dbname", $dbusername, $dbpassword);
+    require_once "rb.php";
+    // localhost менять на хост
+    // server1 менять на название бд(mysql)
+    // root менять на пользователя
+    // последнее менять на пароль
+    R::setup( 'mysql:host=51.81.57.217;dbname=s3265530_southtestes','u3265530_KF5ci9bGmU', '7FV!kWuM4mhn1.HdQ.m6Kpz5' );
 
 
 
