4b69a0a0f4dafd4907fd95e1c4776b4a6976ef7c
FallenX <gyygg2323@gmail.com>
2025-10-19 11:37:34 -0300

    Update connect.php

--- api/Requests/vendor/connect.php
+++ api/Requests/vendor/connect.php
@@ -5,6 +5,7 @@
     // root менять на пользователя
     // последнее менять на пароль
 
-    R::setup( 'mysql:host=51.81.57.217;dbname=s3265530_southtestes','u3265530_KF5ci9bGmU', 'R@8c6Ch+E6zc^!=zGF5DjSDQ' );
+    R::setup( 'mysql:host=51.81.57.217;dbname=s3265530_southtestes','u3265530_KF5ci9bGmU', '7FV!kWuM4mhn1.HdQ.m6Kpz5' );
+
 
 
