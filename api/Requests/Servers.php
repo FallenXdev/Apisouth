643a1980f4aec771c97c09ea3bd291234da00321
FallenX <gyygg2323@gmail.com>
2025-10-21 10:10:01 -0300

    Update Servers.php

--- api/Requests/Servers.php
+++ api/Requests/Servers.php
@@ -5,11 +5,12 @@
 	  "status": 1,
 	  "recommend": true,
 	  "newstatus": false,
-      "edgar_host": "51.81.57.217",
-      "edgar_port": 23981,
+      "edgar_host": "191.96.224.79",
+      "edgar_port": 8867,
       "id": 0
 	}
 ]
+
 
 
 
