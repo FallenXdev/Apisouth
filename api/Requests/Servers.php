5466d132198423807f174c1299d8f3808ed010e8
FallenX <gyygg2323@gmail.com>
2025-10-27 13:19:56 -0300

    Update Servers.php

--- api/Requests/Servers.php
+++ api/Requests/Servers.php
@@ -8,18 +8,9 @@
       "edgar_host": "191.96.224.79",
       "edgar_port": 7917,
       "id": 0
-	},
-	{
-      "name": "South MOBILE #2",
-	  "color": "fff3AAD9",
-	  "status": 1,
-	  "recommend": true,
-	  "newstatus": false,
-      "edgar_host": "191.96.224.79",
-      "edgar_port": 7917,
-      "id": 1
 	}
 ]
+
 
 
 
