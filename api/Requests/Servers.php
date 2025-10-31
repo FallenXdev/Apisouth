df34feda86f46a01308495eccbfcb0ae733bd7c2
FallenX <gyygg2323@gmail.com>
2025-10-31 18:01:25 -0300

    Update Servers.php

--- api/Requests/Servers.php
+++ api/Requests/Servers.php
@@ -8,8 +8,19 @@
       "edgar_host": "51.81.166.66",
       "edgar_port": 35747,
       "id": 0
+	},
+	{
+      "name": "Nordeste MOBILE #2",
+	  "color": "ff33AAD9",
+	  "status": 1,
+	  "recommend": true,
+	  "newstatus": false,
+      "edgar_host": "51.81.166.66",
+      "edgar_port": 35747,
+      "id": q
 	}
 ]
+
 
 
 
